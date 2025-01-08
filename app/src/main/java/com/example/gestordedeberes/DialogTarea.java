37f4701759a6de8b61bb9eb8f4b1968f6ba2a7f3
XanCort <xan.cortes@gmail.com>
2025-01-08 16:48:37 +0100

    Arreglado el calendar dialog

--- app/src/main/java/com/example/gestordedeberes/DialogTarea.java
+++ app/src/main/java/com/example/gestordedeberes/DialogTarea.java
@@ -1,11 +1,15 @@
 package com.example.gestordedeberes;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,24 +19,29 @@ public class DialogTarea extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setPositiveButton("Si", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Log.i("PRUEBA","FLIPA LOCO");
-            }
-        });
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Log.i("PRUEBA","PERO MEU");
-            }
-        });
-        builder.setTitle("Nueva Tarea");
         LayoutInflater layoutInflater = requireActivity().getLayoutInflater();
-        builder.setView(layoutInflater.inflate(R.layout.fragment_tarea,null));
+        View miVista = layoutInflater.inflate(R.layout.fragment_tarea, null);
+
+        builder.setView(miVista);
+        EditText campoFecha = miVista.findViewById(R.id.editTextDate);
+
+        campoFecha.setShowSoftInputOnFocus(false);
+
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                getContext(),
+                (view, year,month,dayOfMonth)->{
+                },
+                2024,12,18
+        );
+
+
+        campoFecha.setOnClickListener(e->{
+            datePickerDialog.show();
+        });
 
         return builder.create();
-
     }
 }
